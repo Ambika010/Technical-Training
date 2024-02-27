852366d2ccfa556b67f51d16d8da10d2bcb9e045
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-02-27 23:12:00 +0530

    Add files via upload

---
+++ Practice Questions/Question_13.java
@@ -0,0 +1,15 @@
+public class Question_13 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int row=1;row<=n;row++){
+            int space=n-row;
+            for(int s=1;s<=space;s++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row;col++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
