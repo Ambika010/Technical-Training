189704f3436c6346dd6f1967af0011dcd4cdb6df
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-03-14 23:25:08 +0530

    Add files via upload

---
+++ Recursion/RecursionDemo.java
@@ -0,0 +1,27 @@
+public class RecursionDemo{
+    //given problem - big problem
+    //but in recursion we think about small problems
+    //printing name 5 time is a big problem  whereas n-1 is a small program
+    static void show(int num){
+        //base case --> termination case
+        if(num==0){
+            return; //exit from function 
+        }
+        System.out.println("Ambika "+num);
+        //num - 1; //small problem+recursion
+        show(num-1);
+        System.out.println("Kumari "+num);
+    }
+    public static void main(String[] args) {
+        show(5);
+    }
+}
+/*     
+show(0)        
+show(1)
+show(2)
+show(3)
+show(4)
+show(5)
+main()
+*/
