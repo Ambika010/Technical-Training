afbcda2a915427378a73c74db5781395567f37e7
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-04-04 02:50:44 +0530

    Update Question_2.java

--- Practice Questions/Question_2.java
+++ Practice Questions/Question_2.java
@@ -13,7 +13,7 @@ public class Question_2 {
         }
         System.out.println((flag==1)?"not prime":"prime");
     }*/
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num=sc.nextInt();
         int count=0;
@@ -23,5 +23,25 @@ public class Question_2 {
             }
         }
         System.out.println((count==2)?"prime":"not prime");
+    }*/
+
+    //suppose num=64
+    //1*64=64  <=>   64*1=64
+    //2*32=64  <=>   32*2=64
+    //4*16=64  <=>   16*4=64
+    //8*8=64         (after 8*8 the factors are repeated)
+    //so to optimise the solution: (x*x=num => pow(x,2)=num => x=sqrt(num))
+    //we will go only till (sqrt(num))
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num=sc.nextInt();
+        int flag=0;
+        for(int i=2;i<=Math.sqrt(num);i++){
+            if(num%i==0){
+                flag=1;
+                break;//exit from loop
+            }
+        }
+        System.out.println((flag==1)?"not prime":"prime");
     }
 }
