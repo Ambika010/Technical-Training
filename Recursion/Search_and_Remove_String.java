854402946234535cc897e74d90308956d181b4c0
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-05-21 02:48:09 +0530

    Add files via upload

---
+++ Recursion/Search_and_Remove_String.java
@@ -0,0 +1,17 @@
+public class Search_and_Remove_String {
+    static String searchAndRemove(String str,char search){
+        if(str.length()==0){
+            return "";
+        }
+        String result = searchAndRemove(str.substring(1), search);
+        if(str.charAt(0)==search){
+            return result;
+        }else{
+            return str.charAt(0)+result;
+        }
+    }
+    public static void main(String[] args) {
+        String result = searchAndRemove("Hello", 'l');
+        System.out.println(result);
+    }
+}
