423628015247bf392ab225a20b1172eea2d59185
Ambika Kumari <145006866+Ambika010@users.noreply.github.com>
2024-06-10 23:26:14 +0530

    Update Longest_SubArray_Sum.java

--- Array/Sliding Window Approach/Longest_SubArray_Sum.java
+++ Array/Sliding Window Approach/Longest_SubArray_Sum.java
@@ -1,1 +1,21 @@
+// calculate the maximum size of the subarray from the given array whose sum is equal to k
 
+public class Longest_SubArray_Sum{
+    public static void main(String[] args) {
+        int arr[] = {3, 1, 2, 7, 4, 2, 1, 1, 5};
+        int k=8;
+        int right=0;
+        int left=0;
+        int currentSum=0;
+        int max=0;
+        for(; right<arr.length; right++){
+            currentSum = currentSum + arr[right];
+            while(currentSum>k){
+                currentSum = currentSum - arr[left];
+                left++;
+            }
+            max = Math.max(max, right - left +1);
+        }
+        System.out.println(max);
+    }
+}
